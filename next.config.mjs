3b7e7e7bc0f51d5d08b5952a4fe36274c6ce67a6
Bagus Wahyu Pratomo <bgswahyup@gmail.com>
2025-06-22 08:06:13 +0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          { key: "Permissions-Policy", value: "interest-cohort=()" }, // blok FLoC
-        ],
-      },
-    ];
-  },
-};
